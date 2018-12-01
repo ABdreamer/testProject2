1df318903a9300c0e32b0081c735f06103e13409
DESKY <DESKY@DESKTOP-K0L1LFS>
2018-12-02 00:00:23 +0530

    added new file to project

---
+++ testing_git/src/testing_git/testing_git_hello.java
@@ -0,0 +1,10 @@
+package testing_git;
+
+public class testing_git_hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+	}
+
+}
